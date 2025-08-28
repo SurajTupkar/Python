214bd4efe59781d26be27ffc20266b3e9ee31b01
Suraj Tupkar <surajtupkar07@gmail.com>
2025-08-29 01:03:32 +0530

    29

--- Tutorials/lect9.py
+++ Tutorials/lect9.py
@@ -1,3 +1,5 @@
+# Dictionaries
+
 dict={
     "name":"XYZ",
     "age" :25,
@@ -23,4 +25,54 @@ dict["update"]="updation_add"
 print(dict)
 
 
+# Nested Dictionaries
 
+nes_dict={
+    "name":"suraj",
+    "subject":{
+        "chem":78,
+        "phy" :59,
+        "math":89
+    }
+}
+
+print(nes_dict["subject"]["phy"])
+
+print(nes_dict)
+
+print(".........................")
+
+# Methods:
+
+print(".......... Methods ...........")
+
+myDict={
+    "name":"XYZ",
+    "age" :25,
+    "topic":"Dict",
+}
+
+
+# 1. keys : return all keys present in the dictionry
+
+print(myDict.keys())
+
+# 2. values : return all values present in the dictionary
+
+print(myDict.values())
+
+# 3. items : return all (key,val) pairs as tuples
+
+print(myDict.items())
+
+# 4. get("keys") : returns the value according to key
+
+print(myDict.get("age"))
+
+# 5. update(new dict) : Insert the specified items to the dictionary
+
+my_Dict1=myDict.update({"subject":"Math"})
+print(myDict)
+
+myDict.update({"subject1":"M2"})
+print(myDict)
