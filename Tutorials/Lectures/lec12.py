c4a1037e17140205d7e1dfa0eab8ef7ceeb36794
Suraj Tupkar <surajtupkar07@gmail.com>
2025-08-30 16:55:29 +0530

    message

---
+++ Tutorials/Lectures/lec12.py
@@ -0,0 +1,26 @@
+# for loops
+
+str="string"
+for i in str:
+    print(i)
+
+
+# Questions:
+
+# print the elements of the following list using a loop
+print("*********************")
+l=[1,4,9,16,25,36,49,64,81,100]
+for i in l:
+    print(i)
+
+
+
+
+
+# Search for a number x in this tuple using loop
+tup=(1,4,9,16,25,36,49,64,81,100)
+
+for i in tup:
+    print(i)
+else:
+    print("**********************")
