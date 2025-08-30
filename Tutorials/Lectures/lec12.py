cddf8e44174e520b733b791de1fa8d3063359945
Suraj Tupkar <surajtupkar07@gmail.com>
2025-08-30 20:53:37 +0530

    message

--- Tutorials/Lectures/lec12.py
+++ Tutorials/Lectures/lec12.py
@@ -32,3 +32,41 @@ print(seq[48])
 
 for i in range(2,5,2):
     print(i)
+
+print("EVEN NUMBER: ")
+for i in range(2,10,2):
+    print(i)
+
+#Practise Questions:
+
+print("**********************")
+
+for i in range(101):
+    print(i)
+print("**********************")
+print("Print Number from 100,1")
+for el in range(100,0,-1):
+    print(el)
+
+count=int(input("Enter Number for Table"))
+for i in range(1,11):
+    print(i*count)
+
+# WAP to find the sum of first n numbers(using while)
+
+n=int(input("Enter the Number:"))
+i=0
+sum=0
+while(i<=n):
+    sum+=i
+    i+=1
+
+print(sum)
+
+
+# WAP to find the factorial of first n number (using for)
+fact=1
+for i in range(1,n+1):
+    fact*=i
+    
+print(fact)
