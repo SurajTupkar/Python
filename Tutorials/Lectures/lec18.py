5d7dd29b7993309b2ad6a718a73f731b6f571837
Suraj Tupkar <surajtupkar07@gmail.com>
2025-09-02 23:45:52 +0530

    message

--- Tutorials/Lectures/lec18.py
+++ Tutorials/Lectures/lec18.py
@@ -24,13 +24,26 @@ constructor:
 
 """
 
-
+#  Class,objects and constructor
 class Student:
+
+    # DM 
     name=""
+
+    #  Class Attribute
+    school_name="ABC"
+
+
+    # constructor
     def __init__(self,fullname):
-        self.name=fullname
+        self.name=fullname                 # Instance Attribute
         print("This is constructor")
         print("Address of self:",self)
+
+    # MF
+    #  Methods
+    def cout(self):
+        print("MF")
     
 
 
@@ -41,3 +54,8 @@ class Student:
 s1=Student("SKT")
 print("Address of s1:",s1)
 print(s1.name)
+s1.cout()
+print(s1.school_name)
+
+s2=Student("SK")
+print(s2.school_name)
