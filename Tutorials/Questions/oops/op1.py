a36b956223a9879eb6349684e9fde4d9893f40ef
Suraj Tupkar <surajtupkar07@gmail.com>
2025-09-03 08:16:10 +0530

    message

--- Tutorials/Questions/oops/op1.py
+++ Tutorials/Questions/oops/op1.py
@@ -1,5 +1,8 @@
-# Create Account class with 2 attributes - balance and account no
-# Create methods for debit,credit & printing the balance
+"""
+Create Account class with 2 attributes - balance and account no
+Create methods for debit,credit & printing the balance
+
+"""
 
 class Account:
     def __init__(self,bal,acc):
