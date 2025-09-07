0d89f5d0ebb0d57cd42a774f275e9b0cd3f71012
Suraj Tupkar <surajtupkar07@gmail.com>
2025-09-07 23:07:41 +0530

    message

--- Tutorials/Practise/Pract10.py
+++ Tutorials/Practise/Pract10.py
@@ -7,7 +7,7 @@ string
 
 """
 
-str="Hello"
+str="HeLlo"
 str1="World"
 str3=str+" "+str1
 print(str3)
@@ -21,6 +21,8 @@ print(str[0])
 #Types 1: Positive Indexing
 print(str[0:1])
 print(str[0:4])
+print("start:stop:step",str[0:5:3]) # Hl
 print(str[:5]) #Hello
 print(str[2:]) #ll0
-print(str[::1])
+print(str[::-1])  # reverse start,stop is blank blank & reverse by -1
+print(str)
