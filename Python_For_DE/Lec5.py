ef79b162833d26a6732a0c7a82e09b4c244c045b
Suraj Tupkar <surajtupkar07@gmail.com>
2025-09-14 19:36:49 +0530

    message

---
+++ Python_For_DE/Lec5.py
@@ -0,0 +1,14 @@
+# Loops
+
+my_list=["Order","Product","Customer"]
+
+# for i in my_list:
+#     print(i)
+#     for x in i:
+#         print(x)
+
+for i in my_list:
+    if(i.lower()=="order"):
+        print("order")
+        break
+
