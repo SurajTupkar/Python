5c2ec21f30c2ae892226de959d0c45e75ce3478d
Suraj Tupkar <surajtupkar07@gmail.com>
2025-09-14 18:56:44 +0530

    message

---
+++ Python_For_DE/Lec3.py
@@ -0,0 +1,24 @@
+"""
+Type Casting
+Types
+1) Explicit
+2) Implicit
+
+
+
+"""
+
+a=10
+print(type(a))
+b=str(a)
+print(type(b))
+print("string:",b)
+c=int(a)
+print(type(c))
+print("int:",c)
+d=float(a)
+print(type(d))
+print("float",d)
+e=bool(a)
+print(type(e))
+print("bool",e)
---
+++ Python_For_DE/Lec4.py
@@ -0,0 +1,10 @@
+# String
+
+str="Hello"
+print(str.isnumeric())
+
+str1="10"
+print(str1.isnumeric())
+
+str2="10abc"
+print(str2.isalnum())
