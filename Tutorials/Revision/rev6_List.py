ec8c2a76fd222c4131ae2d03052941029c257b30
Suraj Tupkar <surajtupkar07@gmail.com>
2026-02-08 16:43:54 +0530

    List_01

--- Tutorials/Revision/rev6_List.py
+++ Tutorials/Revision/rev6_List.py
@@ -1,4 +1,15 @@
 """
+✅ Create list
+✅ Indexing / slicing
+✅ Mutable concept
+✅ append / insert / extend
+✅ remove / pop / clear
+✅ sort / reverse
+✅ Looping
+✅ copy concept
+✅ Nested list
+
+
  list in python
  -> A built in data type that stores set of values.
  -> It can store elements of different types :  int
@@ -53,10 +64,49 @@ print("slicing")
 
 marks=[10,20,30,40,50]
 
-print(marks[1:4]) # 20,30,40
-print(marks[:5])  # 10,20,30,40,50
-print(marks[0:])  # 10,20,30,40,50
-print(marks[0:5:3]) #10,40
-print(marks[-5:-1]) 
+print(marks[1:4])   # 20,30,40
+print(marks[:5])    # 10,20,30,40,50
+print(marks[0:])    # 10,20,30,40,50
+print(marks[0:5:3]) # 10,40
+print(marks[-5:-1]) # 10 20 30 40 50
+print(marks[::-1])  # 50 40 30 20 10
+
+# Modify List Because its Mutable
+marks[0] = 100      # [100,20,30,40,50]
+print(marks)
+
+# Important List Methods
+# 1. Add Elements : append,insert,extend
+
+marks.append(5)      # add 5 at the end
+print(marks)         # [100,20,30,40,50,5]
+
+marks.insert(1,99)   # Add at index : 99 add at index 99 
+print(marks)         # [100,99,20,30,40,50,5]
+
+marks.extend([7,8])  # add many at the end
+print(marks)         # [100,99,20,30,40,50,5,7,8]
+
+# 2. Remove Elements
+
+marks.remove(20)    # remove by value(20)
+print(marks)        # [100,99,30,40,50,5,7,8] : Remove 20 from list
+
+marks.pop()         # remove last element from list 
+print(marks)        # [100,99,30,40,50,5,7]
+
+marks.pop(2)        # remove element by index
+print(marks)        # [100,99,40,50,5,7]
+
+marks.clear()       # empty list : clear all elements from list
+print("Empty List:",marks)        # []
+
+
+
+
+
+
+
+
 
 
