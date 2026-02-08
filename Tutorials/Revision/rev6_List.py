e3bd5d6daf2bda61ccc53c193ef00ab753336497
Suraj Tupkar <surajtupkar07@gmail.com>
2026-02-08 17:27:59 +0530

    message

--- Tutorials/Revision/rev6_List.py
+++ Tutorials/Revision/rev6_List.py
@@ -88,6 +88,7 @@ marks.extend([7,8])  # add many at the end
 print(marks)         # [100,99,20,30,40,50,5,7,8]
 
 # 2. Remove Elements
+# remove,pop,pop(index),clear
 
 marks.remove(20)    # remove by value(20)
 print(marks)        # [100,99,30,40,50,5,7,8] : Remove 20 from list
@@ -98,8 +99,92 @@ print(marks)        # [100,99,30,40,50,5,7]
 marks.pop(2)        # remove element by index
 print(marks)        # [100,99,40,50,5,7]
 
-marks.clear()       # empty list : clear all elements from list
+# marks.clear()       # empty list : clear all elements from list
 print("Empty List:",marks)        # []
+
+# 3. Search & Count
+
+print(marks.index(99))      # position : Find index of value
+print(marks.count("a"))     # Frequency of value
+
+
+# 4. Sort and Reverse
+# sort works only when all elements are same type.
+# if i have list of mixed string and int elements Python cannot compare int with string. so it will crash.
+
+lst=[10,40,70,20,80,"d","a","c","e"]  
+
+"""
+print(marks.sort()) 
+
+it will crash or getting error like TypeError: '<' not supported between instances of 'str' and 'int'
+
+Sorting Means:
+Python compares values:
+10<20   This is ok
+"a"<"b" This is ok
+10<"a"  but this not XXX
+because python cannot compares int with string, so it crashes
+
+When .sort() works
+
+case1 : All Numbers
+Case2 : All Strings
+Case3 : Same Datatype only
+
+"""
+
+#  How to sort mixed list
+
+# Method 1: Separate Numbers and Strings
+
+# nums=[]
+# chars=[]
+
+# for i in lst:
+#     if(type(i)==int):
+#         nums.append(i)
+#     else:
+#         chars.append(i)
+
+# nums.sort()
+# chars.sort()
+
+# print(nums + chars)
+
+
+
+
+
+
+nums  = []
+chars = []
+
+for i in lst:
+    if(type(i)==int):
+        nums.append(i)
+    else:
+        chars.append(i)
+
+nums.sort()
+chars.sort()
+print(nums + chars)
+
+nums.reverse()
+chars.reverse()
+print(nums + chars)
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
